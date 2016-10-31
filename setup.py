1d336540f95eb099cc99f039b1381c41537e1e2d
Diego Blanco <diego.blanco@treitos.com>
2016-10-31 17:16:57 +0100

    Changed version in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ AUTHOR_EMAIL='diego.blanco@treitos.com'
 URL='https://github.com/diego-treitos/ansible-inventory'
 
 
-VERSION="0.1.2"
+VERSION="0.2.0"
 SCRIPTS=['ansible-inventory']
 PACKAGES=[]
 MANIFEST="""
