df7530065c5b505a3f529ba0f091a477f206bafb
Diego Blanco <diego.blanco@treitos.com>
2017-09-08 09:39:54 +0200

    Fixed bug with the symlink system

--- ansible_inventory/globals.py
+++ ansible_inventory/globals.py
@@ -3,7 +3,7 @@
 # vim: set ts=2 sw=2 sts=2 et:
 
 # ( GLOBALS
-VERSION = '0.3.7'
+VERSION = '0.3.8'
 AUTHOR_NAME = 'Diego Blanco'
 AUTHOR_MAIL = 'diego.blanco@treitos.com'
 URL = 'https://github.com/diego-treitos/ansible-inventory'
