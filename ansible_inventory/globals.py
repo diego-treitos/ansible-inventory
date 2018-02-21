2052fdd6ca848f5d26a2a34cc3affc58acf878c7
Diego Blanco <diego.blanco@treitos.com>
2018-02-21 12:26:24 +0100

    Now ansible-inventory accepts lists or dicts as variable values

--- ansible_inventory/globals.py
+++ ansible_inventory/globals.py
@@ -3,7 +3,7 @@
 # vim: set ts=2 sw=2 sts=2 et:
 
 # ( GLOBALS
-VERSION = '0.3.10'
+VERSION = '0.3.11'
 AUTHOR_NAME = 'Diego Blanco'
 AUTHOR_MAIL = 'diego.blanco@treitos.com'
 URL = 'https://github.com/diego-treitos/ansible-inventory'
