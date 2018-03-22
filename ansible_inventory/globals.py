3759979fb23037bdfd1e24711be500fbf4374756
Diego Blanco <diego.blanco@treitos.com>
2018-03-22 18:06:02 +0100

    Version bump v0.5.12

--- ansible_inventory/globals.py
+++ ansible_inventory/globals.py
@@ -2,7 +2,7 @@
 # vim: set ts=2 sw=2 sts=2 et:
 
 # ( GLOBALS
-VERSION = '0.5.7'
+VERSION = '0.5.12'
 AUTHOR_NAME = 'Diego Blanco'
 AUTHOR_MAIL = 'diego.blanco@treitos.com'
 URL = 'https://github.com/diego-treitos/ansible-inventory'
