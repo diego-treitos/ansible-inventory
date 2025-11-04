7fd4d2fd54e81689414da67d0be3fbdca44aaa05
Diego Blanco <diego.blanco@treitos.com>
2025-11-04 21:49:50 +0100

    Fixed exception when managing locks in file backend

--- ansible_inventory/lib.py
+++ ansible_inventory/lib.py
@@ -29,44 +29,45 @@ class AnsibleInventory_InfoException( AnsibleInventory_Exception ):
 
 # SimpleFlock from https://github.com/derpston/python-simpleflock
 class SimpleFlock:
-   """Provides the simplest possible interface to flock-based file locking. Intended for use with the `with` syntax. It will create/truncate/delete the lock file as necessary."""
+  """Provides the simplest possible interface to flock-based file locking. Intended for use with the `with` syntax. It will create/truncate/delete the lock file as necessary."""
 
-   def __init__(self, path, timeout = None):
-      self._path = path
-      self._timeout = timeout
-      self._fd = None
+  def __init__(self, path, timeout = None):
+    self._path = path
+    self._timeout = timeout
+    self._fd = None
 
-   def __enter__(self):
-      self._fd = os.open(self._path, os.O_CREAT)
-      start_lock_search = time.time()
-      while True:
-         try:
-            fcntl.flock(self._fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
-            # Lock acquired!
-            return
-         except (OSError, IOError) as ex:
-            if ex.errno != errno.EAGAIN:  # Resource temporarily unavailable
-               raise
-            elif self._timeout is not None and time.time() > (start_lock_search + self._timeout):
-               # Exceeded the user-specified timeout.
-               raise
+  def __enter__(self):
+    self._fd = os.open(self._path, os.O_CREAT)
+    start_lock_search = time.time()
+    while True:
+      try:
+        fcntl.flock(self._fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        # Lock acquired!
+        return
+      except (OSError, IOError) as ex:
+        if ex.errno != errno.EAGAIN:  # Resource temporarily unavailable
+          raise
+        elif self._timeout is not None and time.time() > (start_lock_search + self._timeout):
+          # Exceeded the user-specified timeout.
+          raise
 
-         # TODO It would be nice to avoid an arbitrary sleep here, but spinning
-         # without a delay is also undesirable.
-         time.sleep(0.1)
+      # TODO It would be nice to avoid an arbitrary sleep here, but spinning
+      # without a delay is also undesirable.
+      time.sleep(0.1)
 
-   def __exit__(self, *args):
+  def __exit__(self, *args):
+    if self._fd:
       fcntl.flock(self._fd, fcntl.LOCK_UN)
       os.close(self._fd)
       self._fd = None
 
-      # Try to remove the lock file, but don't try too hard because it is
-      # unnecessary. This is mostly to help the user see whether a lock
-      # exists by examining the filesystem.
-      try:
-         os.unlink(self._path)
-      except Exception:
-         pass
+    # Try to remove the lock file, but don't try too hard because it is
+    # unnecessary. This is mostly to help the user see whether a lock
+    # exists by examining the filesystem.
+    try:
+      os.unlink(self._path)
+    except Exception:
+      pass
 
 
 # Colors
